168236027782dccd04948aa2165b4c2ece62d1ef
Pepe Maldonado <pepegvf@gmail.com>
2016-12-13 19:23:30 +0100

    primera práctica mongodb terminada

--- mongodb-1/consultas.py
+++ mongodb-1/consultas.py
@@ -11,12 +11,17 @@
 
 from bottle import run, get, request, template
 from pymongo import MongoClient
+from os import linesep
 
 mongoclient = MongoClient()
 db = mongoclient.giw
 
+#Columnas para las tablas de los ejercicios 2, 3, 4 y 5
+all_table_data = ['Nombre de usuario', 'e-mail', 'Página web', 'Tarjeta de crédito', 'Hash de contraseña', 'Nombre', 'Apellido', 'Dirección', 'Aficiones', 'Fecha de nacimiento']
+mid_table_data = ['id', 'e-mail', 'Fecha de nacimiento']
 
-def getResultsData(c):
+#Función que recibe un cursor de mongo y prepara una lista para luego mostrarla por html
+def get_results_data(c):
     data = []
     #Por cada elemento en el cursor devuelto en la consulta
     for r in c:
@@ -25,24 +30,23 @@ def getResultsData(c):
         userData.append(r['email'])
         userData.append(r['webpage'])
 
-        creditCardData = "Número: " + r['credit_card']['number'] + '\n'
-        creditCardData += "Fecha de expiración: " + r['credit_card']['expire']['month'] + '/' + \
-                          r['credit_card']['expire']['year']
+        creditCardData = "Número: " + r['credit_card']['number'] + linesep
+        creditCardData += "Fecha de expiración: " + r['credit_card']['expire']['month'] + '/' + r['credit_card']['expire']['year']
         userData.append(creditCardData)
 
         userData.append(r['password'])
         userData.append(r['name'])
         userData.append(r['surname'])
 
-        addressData = "Pais: " + r['address']['country'] + '\n'
-        addressData += "Zip: " + r['address']['zip'] + '\n'
-        addressData += "Calle: " + r['address']['street'] + '\n'
+        addressData = "Pais: " + r['address']['country'] + linesep
+        addressData += "Zip: " + r['address']['zip'] + linesep
+        addressData += "Calle: " + r['address']['street'] + linesep
         addressData += "Num: " + r['address']['num']
         userData.append(addressData)
 
         likesData = ''
         for like in r['likes']:
-            likesData += str(like) + '\n'
+            likesData += str(like) + linesep
         userData.append(likesData)
 
         userData.append(r['birthdate'])
@@ -51,21 +55,41 @@ def getResultsData(c):
     return data
 
 
-def validateParameters(argsList):
+#Función que recibe una lista con los argumentos que deberían haber llegado al servidor
+# también recibe un variable que dice si todos los argumentos son obligatorios o no
+def validate_arguments(args_list, all_needed=False):
     args = request.query
-    invalidArgs = []
+    invalid_args = []
+    valid_args = []
     # Comprueba que todos los argumentos pasados son válidos
     for a in args:
         # Si no es válido lo añade a la lista de argumentos inválidos
-        if a not in argsList:
-            invalidArgs.append(a)
-    return invalidArgs
+        if a not in args_list:
+            invalid_args.append(a)
+        #Si no lo mete en la lista de argumentos válidos
+        else:
+            valid_args.append(a)
 
+    # print("Lenght args: ", len(args))
+    # print("invalid arguments: ", invalid_args)
+    # print("invalid arguments length: ", len(invalid_args))
+    # print("valid arguments: ", valid_args)
+    # print("valid arguments lenght: ", len(valid_args))
 
-def showArgsError(invalidArgs):
+    if len(invalid_args) != 0:
+        return False, show_args_error(invalid_args)
+    elif not all_needed and len(valid_args) > 0:
+        return True, ''
+    elif all_needed and len(valid_args) == len(args) and len(args) > 0:
+        return True, ''
+    else:
+        return False, "<p style='color:red'>No se han recibido los argumentos necesarios</p>"
+
+#Función que muestra un mensaje de error con los argumento inválidos
+def show_args_error(invalid_args):
     out = "<p style='color:red'>Argumentos inválidos:</p>\n"
     out += "<ul>"
-    for i in invalidArgs:
+    for i in invalid_args:
         out += "<li>" + i + "</li>"
     out += "</ul>"
     return out
@@ -74,51 +98,52 @@ def showArgsError(invalidArgs):
 @get('/find_user')
 def find_user():
     # http://localhost:8080/find_user?username=burgoscarla
-    #Coge el nombre de usuario de la petición GET
-    username = request.query.username
-    c = db.usuarios
-    user = {"_id":username}
-    #Busca todos un único usuario con ese id
-    res = c.find_one(user)
-    #Si existe dicho usuario rellena las listas con los datos de la BD
-    if res:
-        #Lista para datos simples
-        simple_data = list()
-        #Lista para todos los datos de dirección
-        address = list()
-        #Lista para todos los datos de la tarjeta de crédito
-        credit_card = list()
-        #Lista de todo lo que le gusta al usuario
-        likes = list()
-        for key, value in res.items():
-            if key == 'credit_card':
-                credit_card.append('month : ' + value['expire']['month'])
-                credit_card.append('year : ' + value['expire']['year'])
-                credit_card.append('number : ' + value['number'])
-            elif key == 'address':
-                for k, v in value.items():
-                    address.append(k + ' : ' + v)
-            elif key == 'likes':
-                for l in value:
-                    likes.append(l)
-            else:
-                simple_data.append(key + ' : ' + value)
-        return template('datos', title=username, simple_data=simple_data,
-                        address=address, credit_card=credit_card, likes=likes)
-    #Si no existe devuelve un error
+    valid, msg = validate_arguments(['username'], all_needed=True)
+    if valid:
+        #Coge el nombre de usuario de la petición GET
+        username = request.query.username
+        c = db.usuarios
+        #Busca todos un único usuario con ese id
+        res = c.find_one({"_id":username})
+        #Si existe dicho usuario rellena las listas con los datos de la BD
+        if res:
+            #Lista para datos simples
+            simple_data = list()
+            #Lista para todos los datos de dirección
+            address = list()
+            #Lista para todos los datos de la tarjeta de crédito
+            credit_card = list()
+            #Lista de todo lo que le gusta al usuario
+            likes = list()
+            for key, value in res.items():
+                if key == 'credit_card':
+                    credit_card.append('month : ' + value['expire']['month'])
+                    credit_card.append('year : ' + value['expire']['year'])
+                    credit_card.append('number : ' + value['number'])
+                elif key == 'address':
+                    for k, v in value.items():
+                        address.append(k + ' : ' + v)
+                elif key == 'likes':
+                    for l in value:
+                        likes.append(l)
+                else:
+                    simple_data.append(key + ' : ' + value)
+            return template('datos', title=username, simple_data=simple_data,
+                            address=address, credit_card=credit_card, likes=likes)
+        #Si no existe devuelve un error
+        else:
+            return "<p style='color:red'>El usuario <strong>" + username + " </strong> no existe en la BD.</p>"
     else:
-        return "<p style='color:red'>El usuario <strong>" + username + " </strong> no existe en la BD.</p>"
-
+        return msg
 
 @get('/find_users')
 def find_users():
     # http://localhost:8080/find_users?name=Luz
     # http://localhost:8080/find_users?name=Luz&surname=Romero
     # http://localhost:8080/find_users?name=Luz&food=hotdog
-
-    invalidArgs = validateParameters(('name', 'surname', 'birthday'))
-    #Si no hay ningún elemento inválido procede con la consulta
-    if len(invalidArgs) == 0:
+    valid, msg = validate_arguments(['name', 'surname', 'birthday'])
+    if valid:
+        #Si no hay ningún elemento inválido procede con la consulta
         name = request.query.name
         surname = request.query.surname
         birth = request.query.birthday
@@ -132,59 +157,117 @@ def find_users():
             data['birthdate'] = birth
         c = db.usuarios
         res = c.find(data)
-        data = getResultsData(res)
-        return template('table', title="Número de resultados: " + str(res.count()), rows=data)
-    #Si hay alguno inválido muestra el error
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=all_table_data, rows=data)
     else:
-        return showArgsError(invalidArgs)
+        return msg
 
 @get('/find_users_or')
 def find_users_or():
     # http://localhost:8080/find_users_or?name=Luz&surname=Corral
-    invalidArgs = validateParameters(('name', 'surname', 'birthday'))
+
+    valid, msg = validate_arguments(['name', 'surname', 'birthday'])
     # Si no hay ningún elemento inválido procede con la consulta
-    if len(invalidArgs) == 0:
+    if valid:
         name = request.query.name
         surname = request.query.surname
         birth = request.query.birthday
         # Diccionario donde van a ir los datos a buscar
-        data = dict()
+        data = []
         if name:
-            data['name'] = name
+            data.append({'name': name})
         if surname:
-            data['surname'] = surname
+            data.append({'surname': surname})
         if birth:
-            data['birthdate'] = birth
+            data.append({'birthdata': birth})
         c = db.usuarios
-        res = c.find({'$or', data})
-        data = getResultsData(res)
-        return template('table', title="Número de resultados: " + str(res.count()), rows=data)
-    # Si hay alguno inválido muestra el error
+        res = c.find({'$or': data})
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=all_table_data, rows=data)
     else:
-        return showArgsError(invalidArgs)
+        return msg
                
 @get('/find_like')
 def find_like():
     # http://localhost:8080/find_like?like=football
-    pass
-
+    valid, msg = validate_arguments(['like'], all_needed=True)
+    # Si no hay ningún elemento inválido procede con la consulta
+    if valid:
+        like = request.query.like
+        c = db.usuarios
+        res = c.find({'likes': like})
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=all_table_data, rows=data)
+    else:
+        return msg
 
 @get('/find_country')
 def find_country():
     # http://localhost:8080/find_country?country=Irlanda
-    pass
-    
-    
+    valid, msg = validate_arguments(['country'], all_needed=True)
+    # Si no hay ningún elemento inválido procede con la consulta
+    if valid:
+        country = request.query.country
+        c = db.usuarios
+        res = c.find({'address.country': country})
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=all_table_data, rows=data)
+    else:
+        return msg
+
+
 @get('/find_email_birthdate')
 def email_birthdate():
     # http://localhost:8080/find_email_birthdate?from=1973-01-01&to=1990-12-31
-    pass
-    
+    valid, msg = validate_arguments(['from', 'to'], all_needed=True)
+    # Si no hay ningún elemento inválido procede con la consulta
+    if valid:
+        from_date = request.query['from']
+        to_date = request.query.to
+        c = db.usuarios
+        # Fecha de nacimiento mayor que fromDate y menor que toDate
+        user = {'birthdate': {'$gt': from_date, '$lt': to_date}}
+        # query que busca las fechas de nacimiento ordenadas por fecha de nacimiento y por _id
+        res = c.find(user).sort([('birthdate', 1), ('_id', 1)])
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=mid_table_data, rows=data)
+    else:
+        return msg
     
 @get('/find_country_likes_limit_sorted')
 def find_country_likes_limit_sorted():
     # http://localhost:8080/find_country_likes_limit_sorted?country=Irlanda&likes=movies,animals&limit=4&ord=asc
-    pass
+    valid, msg = validate_arguments(['country', 'likes', 'limit', 'ord'], all_needed=True)
+    # Si no hay ningún elemento inválido procede con la consulta
+    if valid:
+        country = request.query.country
+        likes = request.query.likes
+        limit = request.query.limit
+        order = request.query.ord
+        # Almacenamos en una lista todos los likes q se pasan por parametro. Hacemos lista para que $all pueda leer bien.
+        gustos = []
+        cadena = ""
+        for i in likes:
+            if i != ',':
+                cadena += i
+            else:
+                gustos.append(cadena)
+                cadena = ""
+        gustos.append(cadena)
+        # en funcion del tipo de ordenacion se le da un valor entero a la variable order
+        if order == 'asc':
+            order = 1
+        elif order == 'desc':
+            order = -1
+
+        c = db.usuarios
+        user = {'$and': [{'address.country': country}, {'likes': {'$all': gustos}}]}
+        # query que busca en funcion de un country y de los gustos ordenando por fechas de nacimiento y con limite = limit
+        res = c.find(user).sort('birthdate', int(order)).limit(int(limit))
+        data = get_results_data(res)
+        return template('table', num_results=str(res.count()), table_titles=all_table_data, rows=data)
+    else:
+        return msg
 
     
 if __name__ == "__main__":
--- mongodb-1/table.tpl
+++ mongodb-1/table.tpl
@@ -1,7 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-  <title>{{title}}</title>
   <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Latest compiled and minified CSS -->
@@ -13,20 +12,13 @@
   </style>
 </head>
 <body>
-    <div class='container'>
-        <h3>{{title}}</h3>
+    <div class='container-fluid'>
+        <h3>Número de resultados: {{num_results}}</h3>
         <table class='table table-striped table-bordered table-hover'>
         <tr>
-            <th>Nombre de usuario</th>
-            <th>e-mail</th>
-            <th>Página web</th>
-            <th>Tarjeta de crédito</th>
-            <th>Hash de contraseña</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Dirección</th>
-            <th>Aficiones</th>
-            <th>Fecha de nacimiento</th>
+            %for title in table_titles:
+            <th>{{title}}</th>
+            %end
         </tr>
         %for row in rows:
         <tr>
